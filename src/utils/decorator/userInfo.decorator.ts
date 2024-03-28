5f7071c8d517108f4318ba79f135360805882c80
유저이름 <ahwlsqja1324@gmail.com>
2024-03-28 22:13:42 +0900

    feat:userInfo plus

--- src/utils/decorator/userInfo.decorator.ts
+++ src/utils/decorator/userInfo.decorator.ts
@@ -1,6 +1,18 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { ExecutionContext, InternalServerErrorException, createParamDecorator } from "@nestjs/common";
+import { UserInfos } from "src/users/entities/user-info.entity";
 
-export const UserInfo = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
-  return request.user ? request.user : null;
-});
+export const UserInfo = createParamDecorator((data: keyof UserInfos | undefined, context: ExecutionContext) => {
+    const req = context.switchToHttp().getRequest();
+
+    const userInfo = req.user as UserInfos
+    
+    if(!userInfo){
+        throw new InternalServerErrorException('Request에 user 프로터티가 존재하지 않습니다!');
+    }
+
+    if(data){
+        return userInfo[data]
+    }
+    
+    return userInfo;
+});  
