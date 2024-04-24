12a150eff08b378bc1190f58eed054acd4af2d79
semin <a82727282@gmail.com>
2024-04-24 15:34:23 +0900

    docs

---
+++ src/cache/redis.service.ts
@@ -0,0 +1,63 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import * as Redis from 'ioredis';
+
+@Injectable()
+export class RedisService implements OnModuleDestroy {
+  private cluster: Redis.Cluster;
+
+  constructor() {
+    this.cluster = new Redis.Cluster(
+      [
+        {
+          host: process.env.REDIS_HOST_1,
+          port: parseInt(process.env.REDIS_PORT_1, 10),
+        },
+        {
+          host: process.env.REDIS_HOST_2,
+          port: parseInt(process.env.REDIS_PORT_2, 10),
+        },
+        {
+          host: process.env.REDIS_HOST_3,
+          port: parseInt(process.env.REDIS_PORT_3, 10),
+        },
+      ],
+      {
+        natMap: {
+          '172.22.0.7:6380': {
+            host: process.env.REDIS_HOST,
+            port: parseInt(process.env.REDIS_PORT_1),
+          },
+          '172.22.0.5:6381': {
+            host: process.env.REDIS_HOST,
+            port: parseInt(process.env.REDIS_PORT_2),
+          },
+          '172.22.0.2:6382': {
+            host: process.env.REDIS_HOST,
+            port: parseInt(process.env.REDIS_PORT_3),
+          },
+          '172.22.0.3:6383': {
+            host: process.env.REDIS_HOST,
+            port: parseInt(process.env.REDIS_PORT_4),
+          },
+          '172.22.0.6:6384': {
+            host: process.env.REDIS_HOST,
+            port: parseInt(process.env.REDIS_PORT_5),
+          },
+          '172.22.0.4:6385': {
+            host: process.env.REDIS_HOST,
+            port: parseInt(process.env.REDIS_PORT_6),
+          },
+        },
+        scaleReads: 'slave',
+      },
+    );
+  }
+
+  getCluster(): Redis.Cluster {
+    return this.cluster;
+  }
+
+  onModuleDestroy() {
+    this.cluster.disconnect();
+  }
+}
